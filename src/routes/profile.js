bad3330246f548ba64a1b307f8e1333648da3713
anirudhece2020 <anirudhece2020@gmail.com>
2025-05-31 11:09:00 +0000

    added `profile/change-password` api.

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { userAuth } from "../middlewares/auth.js";
 import { validateProfileEditData } from "../utils/validation.js";
+import bcrypt from "bcrypt";
+import validator from "validator";
 
 const profileRouter = express.Router();
 
@@ -28,10 +30,44 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
 
     await logedInUser.save();
     res.status(200).send(logedInUser);
-
   } catch (error) {
     console.log(`error happened inside profiles 🔴:`, error);
-    res.status(500).send('unable to update the userProfile');
+    res.status(500).send("unable to update the userProfile");
+  }
+});
+
+profileRouter.patch("/profile/change-password", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const { newPassword } = req.body;
+
+    if (!newPassword) {
+      return res.status(400).send("New password is required.");
+    }
+
+    const isSamePassword = await bcrypt.compare(
+      newPassword,
+      loggedInUser.password
+    );
+    if (isSamePassword) {
+      return res
+        .status(400)
+        .send("New password cannot be the same as the old password.");
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+      return res.status(400).send("New password should be strong.");
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    loggedInUser.password = hashedPassword;
+    await loggedInUser.save();
+
+    res.status(200).send('password changed successfully');
+
+  } catch (error) {
+    console.error(`Error in /profile/change-password:`, error);
+    res.status(500).send("Unable to change password.");
   }
 });
 
