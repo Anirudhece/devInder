16bc7cb6b690239a8a5a7e437d43c780f9626c84
anirudhece2020 <anirudhece2020@gmail.com>
2025-06-10 17:40:24 +0000

    auth file added

--- frontend/src/api_handlers/auth.ts
+++ frontend/src/api_handlers/auth.ts
@@ -1,11 +1,38 @@
 import axios from "axios";
 import { HEADERS } from "../utils/getHeader";
-const callLoginApi = async (username: string, password: string) => {
+
+// Interfaces
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  age: number;
+  gender: string;
+  __v: number;
+  skills: any[];
+}
+
+interface LoginDataInterface {
+  msg: string;
+  user: User;
+}
+
+const callLoginApi = async (
+  username: string,
+  password: string
+): Promise<LoginDataInterface | undefined> => {
   try {
     const endPoint = `${process.env.NEXT_PUBLIC_BACKENDHOST}/signin`;
-    await axios.post(endPoint, { email: username, password }, { headers: HEADERS });
+    const response = await axios.post<LoginDataInterface>(
+      endPoint,
+      { email: username, password },
+      { headers: HEADERS, withCredentials: true }
+    );
+    return response.data;
   } catch (error: any) {
-    console.error(error?.response?.data || error.message);
+    console.log(error?.response?.data || error.message);
   }
 };
 
