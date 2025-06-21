bd6110cf7fbfeb7c108775ed19b78229a95af290
anirudhece2020 <anirudhece2020@gmail.com>
2025-06-21 11:08:32 +0000

    added logout.

--- frontend/src/api_handlers/auth.ts
+++ frontend/src/api_handlers/auth.ts
@@ -4,8 +4,9 @@ import { User } from "@/types/userTypes";
 import { BASE_URL } from "@/utils/constants";
 
 interface LoginDataInterface {
-  msg: string;
-  user: User;
+  msg: string | undefined;
+  user: User | undefined;
+  err?: string;
 }
 
 const callLoginApi = async (
@@ -22,7 +23,24 @@ const callLoginApi = async (
     return response.data;
   } catch (error: any) {
     console.log(error?.response?.data || error.message);
-  }
+    return {
+      msg: undefined, 
+      user: undefined,
+      err: error?.response?.data?.msg ?? 'Something went wrong',
+    };}
 };
 
+const callLogoutApi = async () => {
+  try {
+    const endPoint = `${BASE_URL}/logout`;
+    const response = await axios.post(
+      endPoint,
+      {},
+      { headers: HEADERS, withCredentials: true }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 export default callLoginApi;
+export { callLogoutApi };
