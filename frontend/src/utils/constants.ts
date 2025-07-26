cab78229a47a1df21b0dc12668b31726478ae362
anirudhece2020 <anirudhece2020@gmail.com>
2025-07-26 13:15:34 +0000

    added prod for `BASE_URL` .

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,4 @@
-export const BASE_URL: string =
-  "https://4000-firebase-devinder-1747476670920.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "https://4000-firebase-devinder-1747476670920.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev"
+    : "/api";
