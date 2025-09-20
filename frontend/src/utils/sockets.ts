ce1e1b18bd53b6c39d358c2c2cd24bb355ac3750
anirudhece2020 <anirudhece2020@gmail.com>
2025-09-20 09:10:35 +0000

    new socket configuration.

--- frontend/src/utils/sockets.ts
+++ frontend/src/utils/sockets.ts
@@ -1,7 +1,20 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
-export const socket = io(BASE_URL, {
-  withCredentials: true,
-  autoConnect: false,
-});
+
+const getUrl = () => {
+  if (process.env.NODE_ENV === "development") {
+    return io(BASE_URL, {
+      withCredentials: true,
+      autoConnect: false,
+    });
+  }
+
+  return io(`/`, {
+    path: `/api/socket.io`,
+    withCredentials: true,
+    autoConnect: false,
+  });
+};
+
+export const socket = getUrl();
